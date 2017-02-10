09525ea5f5bb6222ead7dd87d9b1f73363f39bc7
Головенко Максим В <golovenkomv@lab227.local>
2017-02-10 09:19:40 +0300

    newfilegolovenkomv

---
+++ golovenkomv.cpp
@@ -0,0 +1,68 @@
+#include "golovenkomv.h"
+
+/**
+ * Метод Гаусса
+ */
+void golovenkomv::lab1()
+{
+
+}
+
+
+/**
+ * Метод Гаусса с выбором главного элемента
+ */
+void golovenkomv::lab2()
+{
+
+}
+
+
+
+/**
+ * Метод квадратного корня (метод Холецкого)
+ */
+void golovenkomv::lab3()
+{
+
+}
+
+
+
+/**
+ * Метод прогонки
+ */
+void golovenkomv::lab4()
+{
+
+}
+
+
+
+/**
+ * Метод Якоби
+ */
+void golovenkomv::lab5()
+{
+
+}
+
+
+
+/**
+ * Метод Зейделя
+ */
+void golovenkomv::lab6()
+{
+
+}
+
+
+
+/**
+ * Один из градиентных методов
+ */
+void golovenkomv::lab7()
+{
+
+}
