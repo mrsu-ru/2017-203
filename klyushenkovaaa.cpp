56a3e41508142db7f82a98d69d8c75c59bdf73fe
Klyushenkova <romily.myren@yandex.ru>
2017-03-02 16:19:15 +0300

    lab 1,2

--- klyushenkovaaa.cpp
+++ klyushenkovaaa.cpp
@@ -1,38 +1,39 @@
-#include "ivanovii.h"
+#include "klyushenkovaa.h"
 
 /**
  * Метод Гаусса
  */
 void klyushenkovaa::lab1()
 {
-for(int i=0;i<n;i++)
+double kof;
+for(int i=0;i<N;i++)
         {
-            koif=a[i][i];
-            for(int l=i;l<n;l++)
+            kof=A[i][i];
+            for(int l=i;l<N;l++)
             {
-                a[i][l]=a[i][l]/koif;
+                A[i][l]=A[i][l]/kof;
             }
-            b[i]=b[i]/koif;
-            for(int j=i+1;j<n;j++)
+            b[i]=b[i]/kof;
+            for(int j=i+1;j<N;j++)
             {
-                koif=a[j][i];
-                for(int k=i;k<n;k++)
+                kof=A[j][i];
+                for(int k=i;k<N;k++)
                 {
-                    a[j][k]=a[j][k]-a[i][k]*koif;
+                    A[j][k]=A[j][k]-A[i][k]*kof;
                 }
-                b[j]=b[j]-b[i]*koif;
+                b[j]=b[j]-b[i]*kof;
             }
         }
     x[n-1]=b[n-1];
-    x[n-2]=b[n-2] - a[n-2][n-1]*x[n-1];
+    x[n-2]=b[n-2] - A[n-2][n-1]*x[n-1];
     for(int i=n-3;i>=0;i--)
     {
         double sum=0;
-        for(int j=i+1;j<n;j++)
+        for(int j=i+1;j<N;j++)
         {
-            sum+=a[i][j]*x[i];
+            sum+=A[i][j]*x[i];
         }
-        x[i]=(b[i]-sum)/a[i][i];
+        x[i]=(b[i]-sum)/A[i][i];
     }
 
 }
@@ -43,6 +44,54 @@ for(int i=0;i<n;i++)
  */
 void klyushenkovaa::lab2()
 {
+double kof;
+    for(int i=0;i<N;i++)
+    {
+
+
+        int ind=i;
+        double m_el=abs(A[i][i]);
+        for(int q=q+1;q<N;q++)
+        {
+
+            if( abs(A[q][i])>m_el )
+            {
+                m_el=abs(A[q][i]);
+                ind=q;
+            }
+        }
+        for(int indexswap=0;index<N;index++)
+        {
+            swap(A[i][index],A[ind][index]);
+        }
+
+            kof=A[i][i];
+            for(int l=i;l<N;l++)
+            {
+                A[i][l]=N[i][l]/kof;
+            }
+            b[i]=b[i]/kof;
+            for(int j=i+1;j<N;j++)
+            {
+                kof=A[j][i];
+                for(int k=i;k<N;k++)
+                {
+                    A[j][k]=A[j][k]-A[i][k]*kof;
+                }
+                b[j]=b[j]-b[i]*kof;
+            }
+        }
+    x[N-1]=b[N-1];
+    x[N-2]=b[N-2] - A[N-2][N-1]*x[N-1];
+    for(int i=N-3;i>=0;i--)
+    {
+        double sum=0;
+        for(int j=i+1;j<N;j++)
+        {
+            sum+=A[i][j]*x[i];
+        }
+        x[i]=(b[i]-sum)/A[i][i];
+    }
 
 }
 
