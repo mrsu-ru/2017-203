768604df8ca76fd2a7074918ec78d5039d6fd313
Nikita Kuzmin <h0las@outlook.com>
2017-03-29 01:11:07 +0300

    add lab3,4

--- kuzminnal.cpp
+++ kuzminnal.cpp
@@ -89,7 +89,66 @@ void kuzminnal::lab2()
  */
 void kuzminnal::lab3()
 {
+	double** L = new double*[N];
+    for(int i = 0; i < N; i++)
+    {
+        L[i] = new double[N];
+    }
+    double sum = 0;
+    double* y = new double[N];
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < N; j++)
+        {
+            L[i][j] = 0;
+        }
+    }
+    for(int i = 0; i < N; i++)
+    {
+        for(int k = 0; k < i; k++)
+        {
+            sum = sum + L[i][k] * L[i][k];
+        }
+        L[i][i] = sqrt(A[i][i] - sum);
+        sum = 0;
 
+        for(int j = i + 1; j < N; j++)
+        {
+            for(int k = 0; k < j-1; k++)
+            {
+                sum = sum + L[j][k] * L[i][k];
+            }
+            L[j][i] = (A[j][i] - sum) / L[i][i];
+            sum = 0;
+        }
+        x[i] = 0;
+        y[i] = 0;
+    }
+
+    y[0] = b[0] / L[0][0];
+    for(int i = 1; i < N; i++)
+    {
+        for(int j = 0; j < i; j++)
+        {
+            sum = sum + L[i][j]*y[j];
+        }
+        y[i] = (b[i] - sum) / L[i][i];
+        sum = 0;
+    }
+    x[N-1] = y[N-1] / L[N-1][N-1];
+    for(int i = N - 2; i >= 0; i--)
+    {
+        for(int j = i + 1; j < N; j++)
+        {
+            sum = sum + L[j][i] * x[j];
+        }
+        x[i] = (y[i] - sum) / L[i][i];
+        sum = 0;
+    }
+    delete[] y;
+    for(int i = 0; i < N; i++)
+        delete[] L[i];
+    delete[] L;
 }
 
 
@@ -99,7 +158,28 @@ void kuzminnal::lab3()
  */
 void kuzminnal::lab4()
 {
+	double* P = new double[N];
+    double* Q = new double[N];
+    for(int i = 0; i < N; i++)
+    {
+        P[i] = 0;
+        Q[i] = 0;
+    }
+    P[0] = A[0][1] / (-A[0][0]);
+    Q[0] = b[0] / A[0][0];
+    for(int i = 0; i < N; i++)
+    {
+        P[i] = A[i][i+1] / (-A[i][i] - A[i][i-1] * P[i-1]);
+        Q[i] = (-b[i] + A[i][i-1]*Q[i-1]) / (-A[i][i]-A[i][i-1] * P[i-1]);
+    }
+    x[N-1] = Q[N-1];
+    for(int i = N - 2; i >= 0; i--)
+    {
+        x[i] = P[i] * x[i+1] + Q[i];
+    }
 
+    delete[] P;
+    delete[] Q;
 }
 
 
@@ -130,4 +210,14 @@ void kuzminnal::lab6()
 void kuzminnal::lab7()
 {
 
+}
+
+void kuzminnal::lab8()
+{
+
+}
+
+std::string kuzminnal::get_name()
+{
+  return std::string("Кузьмин Никита Александрович");
 }
