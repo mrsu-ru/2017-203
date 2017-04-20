0095ace508b8207ad28e37bef6744ee8d0195b85
Denis Kurbatov <kurb.2014@mail.ru>
2017-04-20 20:59:46 +0300

    labs 3,4

--- kurbatovdi.cpp
+++ kurbatovdi.cpp
@@ -78,7 +78,48 @@ void kurbatovdi::lab2()
  */
 void kurbatovdi::lab3()
 {
+	double** L = new double*[N];
+	for (int i=0; i<N; i++)
+	L[i] = new double[N];
+	double* y = new double[N];
 
+   for (int i=0; i<N; i++)
+   {
+		for (int j=0; j<N; j++)
+			{
+				L[i][j]=0;
+			}
+   }
+
+  for(int i=0;i<N;i++)
+	{
+		for(int k=0;k<=i-1;k++)
+
+			L[i][i]+=L[i][k]*L[i][k];
+			L[i][i]=sqrt(A[i][i]-L[i][i]);
+			for(int j=i+1;j<N;j++)
+				{
+					for(int k=0;k<=i-1;k++)
+						L[j][i]+=L[i][k]*L[j][k];
+						L[j][i]=(A[i][j]-L[j][i])/L[i][i];
+				}
+  }
+	double summa=0;
+    for(int i=0;i<N;i++)
+		{
+			for(int j=0;j<i;j++){
+			summa+=L[i][j]*y[j];}
+			y[i]=(b[i]-summa)/L[i][i];
+			summa=0;
+		}
+	for(int i=N-1;i>=0;i--)
+		{
+			for(int j=i+1;j<N;j++){
+				summa+=L[j][i]*x[j];}
+				x[i]=(y[i]-summa)/L[i][i];
+				summa=0;
+		}
+   delete[] y;
 }
 
 
@@ -88,6 +129,29 @@ void kurbatovdi::lab3()
  */
 void kurbatovdi::lab4()
 {
+	long double* P = new long double[N];
+    long double* Q = new long double[N];
+    for (int i=0; i<N; i++)
+    {
+        P[i]=0;
+        Q[i]=0;
+    }
+
+    P[0]=A[0][1]/(-A[0][0]);
+    Q[0]=b[0]/A[0][0];
+
+    for(int i=1; i<N; i++)
+    {
+        P[i] = A[i][i+1]/(-A[i][i]-A[i][i-1]*P[i-1]);
+        Q[i] = (-b[i] + A[i][i-1]*Q[i-1])/(-A[i][i]-A[i][i-1]*P[i-1]);
+    }
+
+    x[N-1] = Q[N-1];
+    for(int i=N-2; i>=0; i--)
+        x[i] = P[i]*x[i+1]+Q[i];
+
+    delete[] P;
+    delete[] Q;
 
 }
 
